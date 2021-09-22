6956565672d1ad3e5afb5b0e78d037ee5839bce9
heat <heatieasy@qq.com>
2021-09-22 21:50:46 +0800

    first commit

---
+++ app/src/main/java/com/awo/newcameraxtest/MainActivity.java
@@ -0,0 +1,197 @@
+package com.awo.newcameraxtest;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageCapture imageCapture;
+    private File outputDirectory;
+    private ExecutorService cameraExecutor;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 请求相机权限
+        if (allPermissionsGranted()) {
+            startCamera();
+        } else {
+            ActivityCompat.requestPermissions(this, Configuration.REQUIRED_PERMISSIONS,
+                    Configuration.REQUEST_CODE_PERMISSIONS);
+        }
+
+        // 设置拍照按钮监听
+        Button camera_capture_button = findViewById(R.id.camera_capture_button);
+        camera_capture_button.setOnClickListener(v -> takePhoto());
+
+        // 设置照片等保存的位置
+        outputDirectory = getOutputDirectory();
+
+        cameraExecutor = Executors.newSingleThreadExecutor();
+
+
+    }
+
+    private void takePhoto() {
+        // 确保imageCapture 已经被实例化, 否则程序将可能崩溃
+        if (imageCapture != null) {
+            // 创建带时间戳的输出文件以保存图片，带时间戳是为了保证文件名唯一
+            File photoFile = new File(outputDirectory,
+                    new SimpleDateFormat(Configuration.FILENAME_FORMAT,
+                            Locale.SIMPLIFIED_CHINESE).format(System.currentTimeMillis()) + ".jpg");
+
+            // 创建 output option 对象，用以指定照片的输出方式
+            ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions
+                    .Builder(photoFile)
+                    .build();
+
+            // 执行takePicture（拍照）方法
+            imageCapture.takePicture(outputFileOptions,
+                    ContextCompat.getMainExecutor(this),
+                    new ImageCapture.OnImageSavedCallback() {// 保存照片时的回调
+                        @Override
+                        public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                            Uri savedUri = Uri.fromFile(photoFile);
+                            String msg = "照片捕获成功! " + savedUri;
+                            Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show();
+                            Log.d(Configuration.TAG, msg);
+                        }
+
+                        @Override
+                        public void onError(@NonNull ImageCaptureException exception) {
+                            Log.e(Configuration.TAG, "Photo capture failed: " + exception.getMessage());
+                        }
+                    });
+        }
+    }
+
+    private void startCamera() {
+        // 将Camera的生命周期和Activity绑定在一起（设定生命周期所有者），这样就不用手动控制相机的启动和关闭。
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+        cameraProviderFuture.addListener(() -> {
+            try {
+                // 将你的相机和当前生命周期的所有者绑定所需的对象
+                ProcessCameraProvider processCameraProvider = cameraProviderFuture.get();
+
+                // 创建一个Preview 实例，并设置该实例的 surface 提供者（provider）。
+                PreviewView viewFinder = (PreviewView)findViewById(R.id.viewFinder);
+                Preview preview = new Preview.Builder()
+                        .build();
+                preview.setSurfaceProvider(viewFinder.getSurfaceProvider());
+
+                // 选择后置摄像头作为默认摄像头
+                CameraSelector cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
+
+                // 创建拍照所需的实例
+                imageCapture = new ImageCapture.Builder().build();
+
+                // 设置预览帧分析
+                ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                        .build();
+                imageAnalysis.setAnalyzer(cameraExecutor, new MyAnalyzer());
+
+                // 重新绑定用例前先解绑
+                processCameraProvider.unbindAll();
+
+                // 绑定用例至相机
+                processCameraProvider.bindToLifecycle(MainActivity.this, cameraSelector,
+                        preview,
+                        imageCapture,
+                        imageAnalysis);
+
+            } catch (Exception e) {
+                Log.e(Configuration.TAG, "用例绑定失败！" + e);
+            }
+        }, ContextCompat.getMainExecutor(this));
+
+    }
+
+    private boolean allPermissionsGranted() {
+        for (String permission : Configuration.REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == Configuration.REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {// 申请权限通过
+                startCamera();
+            } else {// 申请权限失败
+                Toast.makeText(this, "用户拒绝授予权限！", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
+
+    private File getOutputDirectory() {
+        File mediaDir = new File(getExternalMediaDirs()[0], getString(R.string.app_name));
+        boolean isExist = mediaDir.exists() || mediaDir.mkdir();
+        return isExist ? mediaDir : null;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cameraExecutor.shutdown();
+    }
+
+    static class Configuration {
+        public static final String TAG = "CameraxBasic";
+        public static final String FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS";
+        public static final int REQUEST_CODE_PERMISSIONS = 10;
+        public static final String[] REQUIRED_PERMISSIONS = new String[]{Manifest.permission.CAMERA};
+    }
+
+    private static class MyAnalyzer implements ImageAnalysis.Analyzer{
+
+        @SuppressLint("UnsafeOptInUsageError")
+        @Override
+        public void analyze(@NonNull ImageProxy image) {
+            Log.d(Configuration.TAG, "Image's stamp is " + Objects.requireNonNull(image.getImage()).getTimestamp());
+            image.close();
+        }
+    }
+}
